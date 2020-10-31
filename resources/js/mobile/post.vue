be3b81fac223114d2726256556d66e01b84228d9
Pavel coruchakin <superpromiser@gmail.com>
2020-11-01 02:36:43 +0800

    upload

---
+++ resources/js/mobile/post.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <quesetionViewComponent></quesetionViewComponent>
+    </div>
+</template>
+
+<script>
+import quesetionViewComponent from '../components/chungHua/questionModal'
+export default {
+    components:{
+        quesetionViewComponent,
+    }
+}
+</script>
+
+<style>
+
+</style>
