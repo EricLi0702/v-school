977ec614a30cffc99ef3d9f307716abcb29b2e3c
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2020-11-24 17:39:11 +0800

    nothing

---
+++ resources/js/components/fluorescentplate/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="es-item" @click="navigateToSetDefault">
+            <div class="es-item-left">
+                Default
+            </div>
+            <div class="es-item-right">
+                <Icon type="ios-arrow-forward" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    methods:{
+        navigateToSetDefault(){
+            console.log(`${this.$route.path}?questionType=${this.currentPath.query.questionType}`);
+            // this.$router.push({path:`${this.$route.path}?questionType=${this.currentPath.query.questionType}`});
+        }
+    }
+}
+</script>
