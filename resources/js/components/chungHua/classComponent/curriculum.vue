57a18e301010a7e6021360dcc50ba05f29c16ed8
Pavel coruchakin <superpromiser@gmail.com>
2020-11-26 02:40:14 +0800

    ok

---
+++ resources/js/components/chungHua/classComponent/curriculum.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <contentCompnent></contentCompnent>
+    </div>
+</template>
+
+<script>
+import contentComponent from '../contentComponent'
+export default {
+    components:{a
+        contentComponent,
+    },
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
