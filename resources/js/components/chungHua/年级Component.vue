137edff2417fd2ddd1e632d8e196ebba2c275d3b
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2020-12-08 10:10:06 +0800

    pull

--- resources/js/components/chungHua/年级Component.vue
+++ resources/js/components/chungHua/年级Component.vue
@@ -3,13 +3,11 @@
         <template  v-if="currentPath.query.className == undefined">
             <div v-for="(_class,i) in classes" :key="i" @click="selClass(_class)">
                 <router-link :to="`${currentPath.path}?gradeName=${grade}&className=${_class.id}`">
-                    <div class="es-item">
-                        <div class="es-item-left">
-                            <div class="es-item-info">
+                    <div class="vx-item">
+                        <div class="vx-item-left">
                             {{_class.lessonName}}
-                            </div>
                         </div>
-                        <div class="es-item-right">
+                        <div class="vx-item-right">
                             <div class="ellipsis">{{`老师${_class.teacherCnt},学生${_class.studentCnt}`}}</div>
                             <Icon type="ios-arrow-forward" />
                         </div>
